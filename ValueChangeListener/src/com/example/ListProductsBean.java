ddc75f4497dc4e1d1b1aac7c305f2403f53033e5
tgmrks <tgmrks@gmail.com>
2016-04-24 10:11:49 -0300

    adding ValueChanceListener example

--- ValueChangeListener/src/com/example/ListProductsBean.java
+++ ValueChangeListener/src/com/example/ListProductsBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 
 @ManagedBean
 @SessionScoped  
@@ -14,10 +15,13 @@ public class ListProductsBean{
 	private Product product;
 	private List<Product> products;
 	private Product selectedItem;
-	
+	private String searchProduct;
+	private List<Product> filterProducts;
+
 	public ListProductsBean(){
 		this.product = new Product();
 		this.products = new ArrayList<Product>();
+		this.filterProducts = new ArrayList<Product>();
 	}
 	
 	public void addItem(){
@@ -29,7 +33,6 @@ public class ListProductsBean{
 		this.products.remove(this.selectedItem);
 	}
 	
-	//A 'ActionEvent' parameter is always required for using ActionListener
 	public void checkItem(ActionEvent event){
 		System.out.println("Cheking Item!");
 		if("".equals(this.product.getManufacturer()))
@@ -41,6 +44,20 @@ public class ListProductsBean{
 			return "About?faces-redirect=true";
 		else
 			return "AboutForm?faces-redirect=true";
+	}
+	
+	public void searchProductFilter(ValueChangeEvent event){
+		System.out.println("Searching...");
+		
+		this.filterProducts.clear();
+		
+		for(Product product : this.products){
+			if(product.getProductName() != null && 
+					product.getProductName().toLowerCase().startsWith(event.getNewValue().toString().toLowerCase())){
+				this.filterProducts.add(product);
+			}
+		}
+		
 	}
 
 	public Product getProduct() {
@@ -58,5 +75,17 @@ public class ListProductsBean{
 	public void setSelectedItem(Product selectedItem) {
 		this.selectedItem = selectedItem;
 	}
-			
+	
+		
+	public String getSearchProduct() {
+		return searchProduct;
+	}
+
+	public void setSearchProduct(String searchProduct) {
+		this.searchProduct = searchProduct;
+	}
+
+	public List<Product> getFilterProducts() {
+		return filterProducts;
+	}			
 }
