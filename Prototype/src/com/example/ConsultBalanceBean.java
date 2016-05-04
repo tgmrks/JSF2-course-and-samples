1397201bec2931a23145a125cfd24b4ea34ce29c
tgmrks <tgmrks@gmail.com>
2016-05-03 22:38:22 -0300

    adding Prototype projcet

---
+++ Prototype/src/com/example/ConsultBalanceBean.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ConsultBalanceBean {
+	
+	private List<String> entries = new ArrayList<String>();
+
+	@PostConstruct 
+	public void inicializar() {
+		for (int i = 0; i < 20; i++) {
+			entries.add("");
+		}
+	}
+	
+	public List<String> getEntries() {
+		return entries;
+	}
+		
+}
