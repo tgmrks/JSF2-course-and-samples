de962595a4656305915e33fd759f92e151535bca
tgmrks <tgmrks@gmail.com>
2016-05-08 23:35:36 -0300

    ImmidiateAttribute example

---
+++ ImmidiateAttribute/src/com/example/StockBean.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class StockBean {
+	private Item item = new Item();;
+	private List<Item> itens = new ArrayList<Item>();
+	
+	public void insert(){
+		this.itens.add(this.item);
+		this.item = new Item();
+	}
+	
+	public void clear(){
+		this.itens.clear();
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+	public List<Item> getItens() {
+		return itens;
+	}
+	
+	
+}
