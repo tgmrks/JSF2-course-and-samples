44d9e452dbfbbd005edeac3a040cd271dfb666f0
tgmrks <tgmrks@gmail.com>
2016-06-03 12:35:57 -0300

    facelets template

---
+++ Prototype/src/com/example/view/TestAjaxBean.java
@@ -0,0 +1,18 @@
+package com.example.view;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TestAjaxBean {
+	
+	private String description;
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
